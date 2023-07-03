e4f5df7561f1dd0d60dc8d599f7ccbf609cb43da
valdeveloper <jjavaldz@gmail.com>
2023-07-03 11:06:10 -0400

    changes

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from "../components/navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 export default function Layout() {
   return (
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import Welcome from "../components/Welcome";
+import Carousel from "../components/Carousel";
+import Footer from "../components/Footer";
+
+export default function Layout() {
+  return (
+    <>
+      <div>
+        <Welcome />
+        <Carousel />
+      </div>
+    </>
+  );
+}
