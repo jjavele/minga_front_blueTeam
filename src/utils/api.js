bba5871593cb3cd33aee54defd55e57b8a8d4766
evaaguilera <eva2aguilera0tauda01@gmail.com>
2023-07-04 15:28:56 -0400

    nav

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,15 @@
-import axios from "axios"
+/* import axios from "axios"
 export const apiUrl = "http://localhost:8080/api/"
 export const api = axios.create({baseUrl: apiUrl})
 export const endpoints = {
     read_equipos: "equipos/read",
     sign_in: "signin",
     chapter_form: "chapter-form",
+} */
+import axios from "axios"
+export const apiUrl = "http://localhost:8080/api/"
+export const api = axios.create({baseUrl: apiUrl})
+export const endpoints = {
+    read_equipos: "equipos/read",
+    sign_in: "auth/signin",
 }
