d722843e79b51ba54e86ee037ee0045d309eef59
eva-aguilera <119910190+eva-aguilera@users.noreply.github.com>
2023-07-05 11:58:10 -0400

    Login (#10)
    * front changes
    
    * carpeta router
    
    * nav
    
    * login
    
    ---------
    
    Co-authored-by: valdeveloper <jjavaldz@gmail.com>

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import ButtonWelcome from "./ButtonWelcome"
+import React from "react";
+import ButtonWelcome from "./ButtonWelcome";
 
 export default function Welcome() {
   return (
     <div className="flex h-[100vh] bg-[url('/src/assets/images/backgroundmanga.png')] w-full bg-cover justify-center items-center flex-col lg:bg-[url('/src/assets/images/background.png')] lg:h-[60vh] lg:w-full lg:bg-top">
-        <h1 className="text-white text-3xl mb-3 text-center md:m-10 md:text-5xl lg:m-0">
-          Your favorite comic book store
-        </h1>
-        <p className="text-white text-sm p-2 text-center md:text-xl lg:hidden">
-          From classics to novelties, we have everything you need to immerse
-          yourself in your favorite universes.
-        </p>
-        <p className="text-white text-sm p-2 mb-3 text-center md:text-xl lg:text-1xl lg:m-4">
-          Explore our catalog and live the adventure of your life.
-        </p>
-        <ButtonWelcome name="Let's Go!" to={"/signin"}/>
-        
-      </div>
-  )
+      <h1 className="text-white text-3xl mb-3 text-center md:m-10 md:text-5xl lg:m-0">
+        Your favorite comic book store
+      </h1>
+      <p className="text-white text-sm p-2 text-center md:text-xl lg:hidden">
+        From classics to novelties, we have everything you need to immerse
+        yourself in your favorite universes.
+      </p>
+      <p className="text-white text-sm p-2 mb-3 text-center md:text-xl lg:text-1xl lg:m-4">
+        Explore our catalog and live the adventure of your life.
+      </p>
+      <ButtonWelcome name="Let's Go!" to={"/signin"} />
+    </div>
+  );
 }
 /*
 className="bg-[url('/src/assets/images/backgroundmanga.png')] h-[60vh] w-full bg-cover lg:bg-[url('/src/assets/images/background.png')] lg:h-[60vh] lg:w-full lg:bg-center"
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 
+import ProtectedRoute from "./ProtectedRoute";
+
+
 import { Layout, Login, ChapterForm, Register, MangaForm } from "./index";
 import Home from "../pages/Home";
 
 import NotAllow from "../pages/NotAllow";
+import { element } from "prop-types";
+
 
 
 let token = true;
+
 
 
 
@@ -20,14 +26,25 @@ const router = createBrowserRouter([
         path: "/",
         element: <Home />,
       },
+
       {
         path: "/manga-form",
         element: <MangaForm />
       },
       {
+
         path: "/login",
-        element: <Login />,
+        element: (
+          <ProtectedRoute>
+            <Login />
+          </ProtectedRoute>
+        ),
       },
+      {
+        path: "/:manga_id/chapter-form",
+        element: <ChapterForm />,
+      },
+
       {
         path: "/register",
         element: token ? <Register /> : <Navigate to="*" />
@@ -35,7 +52,7 @@ const router = createBrowserRouter([
       {
         path: "/:manga_id/chapter-form",
         element: <ChapterForm />,
-      },
+   
     ],
   },
   {
